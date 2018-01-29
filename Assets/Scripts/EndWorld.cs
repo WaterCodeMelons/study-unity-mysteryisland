4f2ab0b12b65fb627195dadb2c4b245f9cdf8eef
WlodarczykKamil <kam9611@gmail.com>
2018-01-29 22:10:55 +0100

    no message

---
+++ Assets/Scripts/EndWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndWorld : MonoBehaviour {
+
+    PlayerStatsController player;
+    private float playerHealth;
+    public float EndWorldEnterHealthDecay;
+    public float EndWorldStayHealthDecay;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag=="EndWorldTrigger")
+        {
+            GameObject.Find("Player").GetComponent<PlayerStatsController>().Health(GameObject.Find("Player").GetComponent<PlayerStatsController>().Health() - EndWorldEnterHealthDecay);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "EndWorldTrigger")
+        {
+            GameObject.Find("Player").GetComponent<PlayerStatsController>().Health(GameObject.Find("Player").GetComponent<PlayerStatsController>().Health() - EndWorldStayHealthDecay);
+        }
+    }
+   
+
+
+    
+}
