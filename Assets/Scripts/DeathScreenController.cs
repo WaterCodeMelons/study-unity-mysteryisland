7b058d4a8ef2005dede7eb80ccd8954c6c5337e7
Aleksander Ujek <aleksander.ujek@gmail.com>
2018-01-29 23:37:41 +0100

    End Animation + Dead Screen + MessageSystem

---
+++ Assets/Scripts/DeathScreenController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathScreenController : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("dayValue").GetComponent<Text>().text = "Przetrwałeś "+PlayerPrefs.GetString("dayCount")+" dni";
+	}
+}
